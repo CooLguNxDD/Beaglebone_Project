d15f50eaec53dc925a8d94e2e49aea05961d9502
Andrew <andrewliang0123@gmail.com>
2023-02-02 20:11:07 -0800

    a2d i2c

---
+++ A2DGuide/potDriver.c
@@ -0,0 +1,54 @@
+// Demo application to read analog input voltage 0 on the BeagleBone
+// Assumes ADC cape already loaded by uBoot:
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#include <time.h>
+#include <string.h>
+
+#define A2D_FILE_VOLTAGE0 "/sys/bus/iio/devices/iio:device0/in_voltage3_raw"
+#define A2D_VOLTAGE_REF_V 1.8
+#define A2D_MAX_READING 4095
+int getVoltage0Reading()
+{
+// Open file
+    FILE *f = fopen(A2D_FILE_VOLTAGE0, "r");
+    if (!f) {
+        printf("ERROR: Unable to open voltage input file. Cape loaded?\n");
+        printf(" Check /boot/uEnv.txt for correct options.\n");
+        exit(-1);
+    }
+// Get reading
+    int a2dReading = 0;
+    int itemsRead = fscanf(f, "%d", &a2dReading);
+    if (itemsRead <= 0) {
+        printf("ERROR: Unable to read values from voltage input file.\n");
+        exit(-1);
+    }
+// Close file
+    fclose(f);
+    return a2dReading;
+}
+static void sleepForMs(long long delayInMs)
+{
+    const long long NS_PER_MS = 1000 * 1000;
+    const long long NS_PER_SECOND = 1000000000;
+    long long delayNs = delayInMs * NS_PER_MS;
+    int seconds = delayNs / NS_PER_SECOND;
+    int nanoseconds = delayNs % NS_PER_SECOND;
+    struct timespec reqDelay = {seconds, nanoseconds};
+    nanosleep(&reqDelay, (struct timespec *) NULL);
+}
+
+int main()
+{
+    while (true) {
+        int reading = getVoltage0Reading();
+        double voltage = ((double)reading / A2D_MAX_READING) * A2D_VOLTAGE_REF_V;
+        printf("Value %5d ==> %5.2fV\n", reading, voltage);
+        sleepForMs(100);
+    }
+    return 0;
+}
